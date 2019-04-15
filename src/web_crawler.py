369b6ea84bb99014e67144b5312a716ca068192c
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-04-15 13:10:23 -0600

    need to fix url verify

--- src/web_crawler.py
+++ src/web_crawler.py
@@ -9,35 +9,38 @@ def crawl(url: str, depth: int, visited: list, max_depth: int=3):
 	elif url in visited:
 		pass
 	else:
+		print('\t'*depth + url)
 		depth += 1
 		visited.append(url)
 		soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 		html_lines = soup.prettify().split()
 		urls_to_test = [line for line in html_lines if 'href' in line]
-		url_snatcher(url, urls_to_test)
-		scanned = False
-		while not scanned:
-			pass
+		correct_urls = url_verify(url, urls_to_test)
+		for url in correct_urls:
+			print("URL: ", url)
+			crawl(url, depth, visited, max_depth)
 
 
-def url_snatcher(base_url, url_list):
+def url_verify(base_url, url_list):
+
 	if '//' in base_url:  # https stuff still there
-		base = (base_url.split('//'))[1].split('/')[0] + ((base_url.split('//'))[0])  # gets https and root up to .com/
+		base_url = (base_url.split('//'))[1].split('/')[0] + ((base_url.split('//'))[0])  # gets https and root up to .com/
+	for i in range(len(url_list)):
+		if base_url not in url_list[i]:
+			url_list[i] = base_url + url_list[i]
+	return url_list
 
 
 def main():
 	if len(sys.argv) < 2 or len(sys.argv) > 3:
-		print('USAGE: python3.6 web_crawler.py ABSOLUTE_URL [MAX_SEARCH_DEPTH]')
+		print('\tUSAGE: python3.6 web_crawler.py ABSOLUTE_URL [MAX_SEARCH_DEPTH]')
 		sys.exit(1)
 
 	elif len(sys.argv) == 2:
 		crawl(sys.argv[1], 0, [])
 
-	elif len(sys.argv) == 3:
-		crawl(sys.argv[1], 0, [], int(sys.argv[2]))
-
 	else:
-		pass
+		crawl(sys.argv[1], 0, [], int(sys.argv[2]))
 
 
 if __name__ == '__main__':
